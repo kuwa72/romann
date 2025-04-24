e6ff64e23cea3027e5a600f7d816ad702829e703
YOURNAME <ykuwashima@gmail.com>
2025-04-24 20:12:49 +0900

    Bump version to 0.1.1 and update requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="romann",
-    version="0.1.0",
+    version="0.1.1",
     description="日本語→自然なローマ字・英語変換ライブラリ（kakasi/SudachiPyベース＋外来語辞書対応）",
     author="ykuwa",
     author_email="",
